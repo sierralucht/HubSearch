7e138870ce17d6492b132b1b2260e20abd806989
sierralucht <sierralucht@gmail.com>
2017-04-09 13:45:57 -0500

    nonononono

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from github import Github
 
 app = Flask(__name__)
-app.secret_key = "changememememe"
+app.secret_key = "changenonononono"
 
 
 def read_credentials():
