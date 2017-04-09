e7a2214877b0a471755d5f6dc576f92d64aa69f9
Nathan Walsh <nwalsh1995@gmail.com>
2017-04-09 13:42:08 -0500

    add template and logic

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-
+from flask import Flask, render_template, redirect, url_for, request
 from github import Github
 
-with open("../p.txt", "r") as f:
-    lines = f.readlines()
-    user = lines[0].strip()
-    pw = lines[1].strip()
+app = Flask(__name__)
+app.secret_key = "changememememe"
 
-g = Github(user, pw)
 
-for repo in g.get_user().get_repos():
-    print repo.name
+def read_credentials():
+    with open("../p.txt", "r") as f:
+        lines = f.readlines()
+        user = lines[0].strip()
+        pw = lines[1].strip()
+
+    return user, pw
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    user, pw = read_credentials()
+    message = "Failure to login"
+    try:
+        g = Github(user, pw)
+        message = "Logged in"
+    except:
+        message="Failure to login"
+
+    return render_template('index.html', message=message)
+
+if __name__ == "__main__":
+    app.run()
