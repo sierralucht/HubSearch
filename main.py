27bf5d8c2b3b772ffd3c595cf0b9e21336973af0
Nathan Walsh <nwalsh1995@gmail.com>
2017-04-09 13:21:27 -0500

    Add requirements and flask files

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+from github import Github
+
+with open("../p.txt", "r") as f:
+    lines = f.readlines()
+    user = lines[0].strip()
+    pw = lines[1].strip()
+
+g = Github("nwalsh1995", pw)
+
+for repo in g.get_user().get_repos():
+    print repo.name
